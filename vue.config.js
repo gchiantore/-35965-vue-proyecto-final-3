24b0dbf69141528c07ef39616ef65265553cebc9
gchiantore <102333574+gchiantore@users.noreply.github.com>
2023-01-31 15:23:49 -0300

    Proyecto Final Ver: 1.0.4

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-}),
-module.exports = {  
-  devServer: {
-    proxy: {
-      '^/api': {
-        target: 'http://localhost:8080',
-        ws: true,
-        changeOrigin: true
-      }
-    }
-  }
-}
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/-35965-vue-proyecto-final-3/'
+  : '/'
+})
